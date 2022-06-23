c120d0623551085fdd905da0cbb6cacceb5dfec1
“Razvan <razvan.vacaru@tmnl.nl>
2022-06-23 12:42:32 +0200

    added var to the test

--- myproject/src/main/scala/a/A.scala
+++ myproject/src/main/scala/a/A.scala
@@ -1,5 +1,6 @@
 import scala.concurrent.Future
 object a {
   // the bug is reproduced, the memberKind config is ignored
-  implicit val x: List[Int] = 3 :: 42 :: Nil
+  implicit val x = 3 :: 42 :: Nil
+  implicit var v = 3 :: 42 :: Nil
 }
