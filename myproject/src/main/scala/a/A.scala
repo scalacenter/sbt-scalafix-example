7a558665024c1a1f4f619596551e395bd44b77c0
“Razvan <razvan.vacaru@tmnl.nl>
2022-06-23 12:45:10 +0200

    fix test

--- myproject/src/main/scala/a/A.scala
+++ myproject/src/main/scala/a/A.scala
@@ -1,6 +1,6 @@
 import scala.concurrent.Future
 object a {
-  // the bug is reproduced, the memberKind config is ignored
-  implicit val x = 3 :: 42 :: Nil
-  implicit var v = 3 :: 42 :: Nil
+  // the bug is reproduced, the memberKind config is ignored, implicit val shouldn't be touched
+  implicit val x: List[Int] = 3 :: 42 :: Nil
+  implicit var v: List[Int] = 3 :: 42 :: Nil
 }
