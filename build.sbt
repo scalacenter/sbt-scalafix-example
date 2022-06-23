bd36abb42fe9b2851370d0432b5a21b648e4cbe5
“Razvan <razvan.vacaru@tmnl.nl>
2022-06-23 12:23:02 +0200

    commit before sbt scalafix run

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 lazy val myproject = project.settings(
-  scalaVersion := "2.12.11",
+  scalaVersion := "2.12.15",
   semanticdbEnabled := true,
   semanticdbVersion := scalafixSemanticdb.revision
   // with -Wunused compilation fails
--- myproject/src/main/scala/a/A.scala
+++ myproject/src/main/scala/a/A.scala
@@ -1,5 +1,7 @@
 import scala.concurrent.Future
 object a {
-  // the bug is reproduced, the memberKind config is ignored
-  implicit val x: List[Int] = 3 :: 42 :: Nil
+  // scalafix 0.9.34 still has the bug
+  // scalafix 0.10.1 doesn't have the bug
+  implicit val x = 3 :: 42 :: Nil
+  implicit var v = "3" :: "42" :: Nil
 }
